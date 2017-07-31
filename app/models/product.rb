ed20005f8608423163aeafd16b49bb07ae740e82
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-07-31 14:07:22 -0700

    add field validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :image_url, presence: true, allow_blank: true, format: {
+    with:    %r{\.(gif|jpg|png)\Z}i,
+    message: "must be a URL ofr GIF, JPG or PNG image."
+  }
+
 end
