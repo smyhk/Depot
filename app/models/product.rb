ee6c9736b69f8436fcda501a38978b5b75c84b95
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-08-01 16:31:37 -0700

    add price and image format url tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,10 @@ class Product < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-  validates :image_url, presence: true, allow_blank: true, format: {
+  validates :image_url, presence: true, format: {
+    allow_blank: true,
     with:    %r{\.(gif|jpg|png)\Z}i,
-    message: "must be a URL ofr GIF, JPG or PNG image."
+    message: "must be a URL for GIF, JPG or PNG image."
   }
 
 end
