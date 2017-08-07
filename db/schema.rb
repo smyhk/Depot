528cd9e74f791db5803ee54878357ea951562888
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-08-07 10:58:09 -0700

    combine line item quantity and update test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807171355) do
+ActiveRecord::Schema.define(version: 20170807173517) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
