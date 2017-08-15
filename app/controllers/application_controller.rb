e0ad9104ff0df117b7548ae36129fe51968fa3c3
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-08-14 19:44:50 -0700

    limit access only to authorized users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
 
   include SessionCounter
+  before_action :authorize
 
   protect_from_forgery with: :exception
+
+  private
+
+    def authorize
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
+    end
 
 end
