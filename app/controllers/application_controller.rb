3cfa203ec542ff7ba58365d816e4e761d75674ae
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-08-15 17:39:27 -0700

    set locale from given params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   include SessionCounter
   before_action :authorize
+  before_action :set_i18n_locale_from_parameters
 
   protect_from_forgery with: :exception
 
@@ -10,6 +11,19 @@ class ApplicationController < ActionController::Base
     def authorize
       unless User.find_by(id: session[:user_id])
         redirect_to login_url, notice: "Please log in"
+      end
+    end
+
+  protected
+
+    def set_i18n_locale_from_parameters
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] = "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
       end
     end
 
